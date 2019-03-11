e563e63e63850d6e07adcd68c691a812089d5915
U-gojira\jd <jedrzej.dudkiewicz>
2019-03-11 19:56:43 +0100

    "neatification"

--- go.sh
+++ go.sh
@@ -1,30 +1,30 @@
 PATH=$PATH:$(cygpath 'c:/stuff/maven/bin/')
 
 export PATH
-#echo RUNNING WITH PATH: $PATH
-#java -version
 
-g() {
+do_gensrc() { ## generate sources
 	mvn generate-sources
 }
-c() {
+do_clean() { ## clean source tree
 	rm -rf src/main/java/com/gs/parser/GameScript*
 	mvn clean
 	rm -f *.dot *.raw *.svg *.html *.png *.asm
 	bash bin/gsprof clean
 }
 
-maven_build() {
+do_build() { ## build everything
 	mvn package && ( unzip -c target/gamescript.jar META-INF/MANIFEST.MF 2>/dev/null )
 }
-case "$1"
-in
-	b) b; ;;
-	B) j; ;;
-	c) c; ;;
-	g) g; exit; ;;
-	j) j; exit; ;;
-	a) j ;;
-	*) maven_build ;;
-esac
+do_help() { ## show help
+echo "Help for ${0}:"
+grep -E '^do_[a-z]+[(][)] [{] ##' "$0" | while read cmd junk1 junk2 msg ; do echo $(echo $cmd | cut -f2 -d_ | cut -f1 -d\() - $msg ; done
+}
+for x ; do
+	cmd=do_$x
+	if [ "$(type -t $cmd)" = "function" ] ; then
+		$cmd
+	else
+		do_help
+	fi
+done
 
